7b8c79edeae8a9efed69aaed2556f5975c1f6c61
ella <luuuzyes17@gmail.com>
2020-02-12 18:32:33 -0500

    score calculation
    1. database uploaded
    2. DFS method update (recursive)
    3. vertex score calculation updated

--- Graph.java
+++ Graph.java
@@ -1,83 +1,139 @@
-// Java program to print DFS traversal from a given given graph
+//package org.arpit.java2blog;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 import java.io.*;
-import java.util.*;
-import java.io.File;
 import java.util.Scanner;
 
-// This class represents a directed graph using adjacency list
-// representation
 
-class Graph {
-    private int V;   // No. of vertices
+public class Graph
+{
+    private int count = 0;
+    int Vnum;
+    Node[] nodelist;
+    boolean[] existlist;
 
-    // Array  of lists for Adjacency List Representation
-    private LinkedList<Integer> adj[];
-
-    public int getV() {
-        return V;
-    }
-
-    public LinkedList<Integer>[] getAdj() {
-        return adj;
-    }
-
-    // Constructor
-    Graph(int v) {
-        V = v;
-        adj = new LinkedList[v];
-        for (int i = 0; i < v; ++i) {
-            adj[i] = new LinkedList();
-            //adj[i].add(i); //the first index of adj is the index of such node
+    Graph(int Vnum)
+    {
+        this.Vnum=Vnum;
+        this.nodelist = new Node[Vnum];
+        for (int i = 0; i <  Vnum; i++) {
+            this.nodelist[i] = null;
+        }
+        this.existlist = new boolean[Vnum];
+        for (int i = 0; i < Vnum; i++) {
+            this.existlist[i] = false;
         }
     }
 
-    //Function to add an edge into the graph
-    void addEdge(int v, int w) {
-        adj[v].add(w);  // Add w to v's list.
-    }
 
-    // A function used by DFS
-    void DFSUtil(int v, boolean visited[]) {
-        // Mark the current node as visited and print it
-        visited[v] = true;
-        System.out.print(v + " ");
+    static class Node
+    {
+        int index;
+        boolean visited;
+        List<Node> neighbours;
+        int score;
+        int ancestors;
+        int descendants;
 
-        // Recur for all the vertices adjacent to this vertex
-        Iterator<Integer> i = adj[v].listIterator();
-        while (i.hasNext()) {
-            int n = i.next();
-            if (!visited[n])
-                DFSUtil(n, visited);
+        Node(int data)
+        {
+            this.index=data;
+            this.neighbours=new ArrayList<>();
+            int score = 0;
+            int ancestors = 0;
+            int descendants = 0;
+        }
+        public void addneighbours(Node neighbourNode)
+        {
+            this.neighbours.add(neighbourNode);
+        }
+        public List<Node> getNeighbours() {
+            return neighbours;
+        }
+        public void setNeighbours(List<Node> neighbours) {
+            this.neighbours = neighbours;
         }
     }
 
-    // The function to do DFS traversal. It uses recursive DFSUtil()
-    void DFS(int v) {
-        // Mark all the vertices as not visited(set as
-        // false by default in java)
-        boolean visited[] = new boolean[V];
+    // Recursive DFS
+    public  void dfs(Node node)
+    {
+        System.out.print(node.index + " ");
+        List<Node> neighbours=node.getNeighbours();
+        node.visited=true;
+        for (int i = 0; i < neighbours.size(); i++) {
+            Node n=neighbours.get(i);
+            if(n!=null && !n.visited)
+            {
+                dfs(n);
+            }
+            //implement score calculation
+            count++;
+            n.descendants = count;
+            n.ancestors = Vnum -1-count;
+            n.score = n.ancestors + comb(n.descendants,2);
+        }
+    }
 
-        // Call the recursive helper function to print DFS traversal
-        DFSUtil(v, visited);
+    static int comb(int n , int r)
+    {
+        if( r== 0 || n == r)
+            return 1;
+        else
+            return comb(n-1,r)+comb(n-1,r-1);
+    }
+
+    // Iterative DFS using stack
+    public  void dfsUsingStack(Node node)
+    {
+        Stack<Node> stack=new  Stack<Node>();
+        stack.add(node);
+        while (!stack.isEmpty())
+        {
+            Node element=stack.pop();
+            if(!element.visited)
+            {
+                System.out.print(element.index + " ");
+                element.visited=true;
+            }
+
+            List<Node> neighbours=element.getNeighbours();
+            for (int i = 0; i < neighbours.size(); i++) {
+                Node n=neighbours.get(i);
+                if(n!=null && !n.visited)
+                {
+                    stack.add(n);
+                }
+            }
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
         File file = new File("BarabasiAlbert_n500m1.txt");
         Scanner sc = new Scanner(file);
         Evaluate e = new Evaluate();
-        int nVer = sc.nextInt();
-        Graph g = new Graph(nVer);
+        int V = sc.nextInt();
+        Graph g = new Graph(V);
         while(sc.hasNext()) {
             String str = sc.nextLine();
             String ver[] = str.split(":");
             int index = Integer.parseInt(ver[0]);
+            Node node = new Node(index);
+            g.nodelist[index] = node;
             String ad[] = ver[1].split("\\s+");
             for (int i = 0; i < ad.length; i++) {
-                g.addEdge(index, Integer.parseInt(ad[i]));
+                Node node_temp = null;
+                if (!g.existlist[Integer.parseInt(ad[i])]) {
+                    node_temp = new Node(Integer.parseInt(ad[i]));
+                    g.nodelist[Integer.parseInt(ad[i])] = node_temp;
+                }
+                node_temp = g.nodelist[Integer.parseInt(ad[i])];
+                node.addneighbours(node_temp);
             }
         }
-        g.DFS(0);
-        
+//        g.DFS(0);
 
     }
 }
